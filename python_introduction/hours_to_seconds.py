8d557e0a82f33cf4851cf755d3c61092264c44d7
mannie55 <mannnie55@gmail.com>
2024-06-20 14:29:20 +0100

    Fixed some issues in the hours_to_seconds.py file

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,8 +1,8 @@
 #defined and assigned a value to the variable
-hour = 2
+hours = 2
 
 #calculate the number of seconds in an hour
-seconds = hour * 60 * 60
+seconds = hours * 60 * 60
 
 #print the result
-print(f"{hour} hour is {seconds} seconds")
+print(f"{hours} hour is {seconds} seconds")
