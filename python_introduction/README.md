4b7061e0242cc4e90bae2b0650aa381fdc8c062b
mannie55 <mannnie55@gmail.com>
2024-06-20 11:46:05 +0100

    Created python_introduction directory, added basic_operations.py file and README.md

---
+++ python_introduction/README.md
@@ -0,0 +1,2 @@
+<hi>python_introduction</h1>
+<p><b>basic_operations.py<b>: This python program performs basic arithmetic operations</p>
---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+#Assign specific values to the variables
+number1 = 10
+number2 = 5
+
+#Perform addition, subtraction, and multiplication with the variables
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+#Print the result
+print("Addition of", number1, "and", number2, "is", sum)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
