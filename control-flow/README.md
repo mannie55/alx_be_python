a5c2774e573697fdb7048be42644b46cad66e676
mannie55 <mannnie55@gmail.com>
2024-06-28 14:33:48 +0100

    Added matchcasecalculator.py and updated readme

--- control-flow/README.md
+++ control-flow/README.md
@@ -1,1 +1,2 @@
 weather_advice.py: This program gives weather recommendations<br>
+match_case_calculator.py: This is a simple calculator program<br>
---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,16 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
+
+match operation:
+    case "+":
+        print(f"The result is {num1 + num2}")
+    case "-":
+        print(f"The result is {num1 - num2}")
+    case "*":
+        print(f"The result is {num1 * num2}")
+    case "/":
+        if num2 != 0:
+            print(f"The result is {num1 / num2}")
+        else:
+            print("Cannot divide by zero")
