261e7576a24a7ec20e87d1351db4be2146171eab
mannie55 <mannnie55@gmail.com>
2024-06-28 14:59:48 +0100

    fixed print statement in multiplication_table.py file

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,5 @@
 number = int(input("Enter a number to see its multiplication table: "))
 
 for i in range(1, 11):
-    print(number, "*", i, "=", (number * i))
+    total = number * i
+    print(f"{number} * {i} = {total}")
