31d9dc9ca6d61b5ca173d5807370e852782210ed
mannie55 <mannnie55@gmail.com>
2024-07-05 09:05:46 +0100

    Added arithmetic_operations.py: a function that performs basic arithmetic operation. main.py: the file from which the function is called and executed. check README.md for more info.

---
+++ fns_and_dsa/README.md
@@ -0,0 +1,3 @@
+<h1>fns_and_data<h1>
+
+<b>arithmetic_operations.py:<b> this function performs basic arithmetic operations<br>
---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,15 @@
+def perform_operation(num1: float, num2: float, operation: str):
+    match operation:
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            if num2 != 0:
+                return num1 / num2
+            else:
+                raise ValueError("Cannot divide by zero")
+        case _:
+            print("Cant handle that!")
