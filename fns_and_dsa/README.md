6523068319157964839d7346227befaa575c4e50
mannie55 <mannnie55@gmail.com>
2024-07-05 09:09:54 +0100

    fix: removed float and str for specifying which values to be accepted from the function (arithmetic_operator) parameter.

--- fns_and_dsa/README.md
+++ fns_and_dsa/README.md
@@ -1,3 +1,4 @@
 <h1>fns_and_data<h1>
 
 <b>arithmetic_operations.py:<b> this function performs basic arithmetic operations<br>
+<b>main.py:<b> This is used in testing the function<br>
--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     match operation:
         case "add":
             return num1 + num2
