f473e40bc36b0131ce954f897c69bcced52d92f0
mannie55 <mannnie55@gmail.com>
2024-07-05 09:28:18 +0100

    fix: arithmetic_operation.py; reconstructed nested if statement under the division operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,9 +6,9 @@ def perform_operation(num1, num2, operation):
     elif operation == "multiply":
         return num1 * num2
     elif operation == "divide":
-        if num2 != 0:
-            return num1 / num2
-        else:
+        if num2 == 0:
             raise ValueError("Cant divide by zero")
+        else:
+             return num1 / num2
     else:
         print("Invalid")
