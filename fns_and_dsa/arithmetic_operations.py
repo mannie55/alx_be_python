00787d8128de7856bbc30c26cb8f982a4cc695b7
mannie55 <mannnie55@gmail.com>
2024-07-05 09:25:07 +0100

    fix: arithmetic_operation.py; changed matchcase to if statement.

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,14 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            return num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "multiply":
-            return num1 * num2
-        case "divide":
-            if num2 != 0:
-                return num1 / num2
-            else:
-                raise ValueError("Cannot divide by zero")
-        case _:
-            print("Cant handle that!")
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            raise ValueError("Cant divide by zero")
+    else:
+        print("Invalid")
