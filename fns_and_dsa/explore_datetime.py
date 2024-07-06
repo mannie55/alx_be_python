af79278dda3848f4a8f0e52b9aae7511e6e2391b
mannie55 <mannnie55@gmail.com>
2024-07-06 09:38:41 +0100

    fix: formatted the date and time in the exploredatetime file

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,9 +1,9 @@
-from datetime import datetime
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 def display_current_datetime():
     current = datetime.now()
-    print(f"Current date and time: {current.year}-{current.month:02d}-{current.day:02d} {current.hour:02d}:{current.minute:02d}:{current.second:02d}")
+    format_date_time = current.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current date and time: {format_date_time}")
 display_current_datetime()
 
 
@@ -16,8 +16,3 @@ def calculate_future_date():
     future_date = current_date + delta
     print(future_date)
 calculate_future_date()
-
-
-
-
-#
