58310e85b8ae2c18e73694bcd13ff29f56f13a0f
mannie55 <mannnie55@gmail.com>
2024-07-12 13:36:39 +0100

    fix: added correct statement to the return value of the try block

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,7 +3,7 @@ def safe_divide(numerator, denominator):
         try:
             num1 = float(numerator)
             num2 = float(denominator)
-            return num1 / num2
+            return f"The result of the division is {num1 / num2}"
         except ZeroDivisionError:
             return "Error: Cannot divide by zero."   
         except ValueError:
