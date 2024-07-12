c501029cd9ed12d6ef55e515279575c727b201e7
mannie55 <mannnie55@gmail.com>
2024-07-12 13:30:29 +0100

    create robust_division_calculator.py file and updated readme

--- programming_paradigm/README.md
+++ programming_paradigm/README.md
@@ -1,1 +1,3 @@
 <h1>Programming Paradigms<h1>
+<b>bank_account.py:<b> Create a Simple Bank Account Class
+<b>robust_division_calculator.py:<b> Robust Division Calculator with Command Line Arguments
---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,11 @@
+def safe_divide(numerator, denominator):
+        
+        try:
+            num1 = float(numerator)
+            num2 = float(denominator)
+            return num1 / num2
+        except ZeroDivisionError:
+            return "Error: Cannot divide by zero"   
+        except ValueError:
+            return "Error: Please enter numeric values only."
+            
