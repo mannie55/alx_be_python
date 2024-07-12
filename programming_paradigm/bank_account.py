fb2b83b88eda570e721b650f8b17bc35475c98d0
mannie55 <mannnie55@gmail.com>
2024-07-12 11:30:01 +0100

    resolved

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,17 +1,17 @@
 class BankAccount:
 
-    def __init__(self, initial_balance = 0):
-        self.account_balance = initial_balance
-    
+    def __init__(self, account_balance = 0):
+        self._account_balance = account_balance
+        
     def deposit(self, amount):
-       self.account_balance += amount
+       self._account_balance += amount
 
     def withdraw(self, amount):
-        if self.account_balance >= amount:
-            self.account_balance -= amount
+        if self._account_balance > 0 and self.__account_balance >= amount:
+            self._account_balance -= amount
             return True
         else:
             return False
         
     def display_balance(self):
-       print(f"Current Balance: ${self.account_balance}")
+       print(f"Current Balance: ${self._account_balance}")
--- programming_paradigm/main-0.py
+++ programming_paradigm/main-0.py
@@ -2,7 +2,7 @@ import sys
 from bank_account import BankAccount
 
 def main():
-    account = BankAccount(100)  # Example starting balance
+    account = BankAccount(250)  # Example starting balance
     if len(sys.argv) < 2:
         print("Usage: python main.py <command>:<amount>")
         print("Commands: deposit, withdraw, display")
@@ -14,12 +14,14 @@ def main():
     if command == "deposit" and amount is not None:
         account.deposit(amount)
         print(f"Deposited: ${amount}")
+        account.display_balance()
     elif command == "withdraw" and amount is not None:
         if account.withdraw(amount):
             print(f"Withdrew: ${amount}")
+            account.display_balance()
         else:
             print("Insufficient funds.")
-    elif command == "display":
+    elif command == "display":  
         account.display_balance()
     else:
         print("Invalid command.")
